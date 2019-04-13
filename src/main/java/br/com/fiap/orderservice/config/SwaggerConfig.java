983596e30bbb042e6425d304aef780bff2669467
Adriano M. La Selva <a.moreira@paygo.com.br>
2019-04-13 11:36:23 -0300

    Ajustes na documentação com o swagger

--- src/main/java/br/com/fiap/orderservice/config/SwaggerConfig.java
+++ src/main/java/br/com/fiap/orderservice/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     public Docket greetingApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.ecommerce"))
+                .apis(RequestHandlerSelectors.basePackage("br.com.fiap.orderservice"))
                 .build()
                 .apiInfo(metaData());
     }
--- src/main/java/br/com/fiap/orderservice/model/Order.java
+++ src/main/java/br/com/fiap/orderservice/model/Order.java
@@ -1,6 +1,8 @@
 package br.com.fiap.orderservice.model;
 
 import com.wordnik.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 @Entity
 public class Order {
 
